755c15cb606fc11d152690add8d266eeeecec895
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-10-31 22:40:27 +0100

    wip #9

--- bin/cli.js
+++ bin/cli.js
@@ -2,12 +2,20 @@
 
 const program = require('commander');
 const generateBattleText = require('../lib/generate-battle-text');
+const generateUpcomingText = require('../lib/generate-upcoming-events');
 
 program
   .command('battle <url>')
   .description('Generate texts for Instagram and Twitter for a battle and its winner.')
   .action((url) => {
     generateBattleText(url);
+  });
+
+program
+  .command('upcoming <date>')
+  .description('Generate texts for Instagram and Twitter for upcoming events.')
+  .action((date) => {
+    generateUpcomingText(date);
   });
 
 program.parse(process.argv);
