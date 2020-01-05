352933bfe4e125f946307edd9989bd357105ac2b
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-01-05 22:27:14 +0100

    wip: rankings

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,7 @@
 const program = require('commander');
 const generateBattleText = require('../lib/battle-winner');
 const generateUpcomingText = require('../lib/upcoming-events');
+const generateRankingText = require('../lib/rankings');
 
 program
   .command('battle <url>')
@@ -20,6 +21,15 @@ program
     }
 
     generateUpcomingText(date);
+  });
+
+program
+  .command('ranking')
+  .description('Generate texts for dancer and country rankings.')
+  .option('-t, --top', 'Number of top ranked spots to include.')
+  .option('-w, --what', 'Ranking to use.')
+  .action(cmdObj => {
+    generateRankingText(cmdObj);
   });
 
 program.parse(process.argv);
